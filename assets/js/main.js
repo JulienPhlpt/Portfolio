3b527014b2b0e5b314ac3dd09d38070d2b613a56
Julien Philippot <julien.philippot86@gmail.com>
2022-02-04 12:21:30 +0100

    Portfolio v2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -176,8 +176,8 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
@@ -189,3 +189,7 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+// Copyright Year
+document.querySelector('#copyright__year').innerText = new Date().getFullYear();
